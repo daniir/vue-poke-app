dd31377d546e195e657486f8603bb96ada1cd42c
daniir <danrodas03@gmail.com>
2024-01-05 21:35:04 -0500

    Consumó de composable

--- src/components/pokemons/PokemonCard.vue
+++ src/components/pokemons/PokemonCard.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import { usePokemons } from '../../composables/';
+
+const { favorites,  checkLocalStorage, addFavorites, removeFavorite } = usePokemons();
+
 defineProps<{
     id: string;
     name: string;
 }>();
+
+checkLocalStorage();
+
 </script>
 
 <template>
@@ -11,7 +18,6 @@ defineProps<{
             <img 
                 class="h-36 object-cover"
                 :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`"
-
             />
         </div>
         <div class="bg-white flex flex-col justify-center items-center w-full">
@@ -24,11 +30,28 @@ defineProps<{
                     >
                         Detalle
                     </RouterLink>
-                    <button class="flex items-center p-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-heart" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <button v-if="!favorites.find(e => e === id)"
+                        class="flex items-center p-2"
+                        @click="addFavorites(id)"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                            class="icon icon-tabler icon-tabler-heart"
+                            
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
                         </svg><span class="text-slate-600 hover:text-slate-800">Favoritos</span>
+                    </button>
+                    <button v-else
+                        class="flex items-center p-2"
+                        @click="removeFavorite(id)"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" 
+                            class="icon icon-tabler icon-tabler-heart fill-red-600"
+                            width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+                        </svg><span class="text-slate-600 hover:text-slate-800">Remover</span>
                     </button>
             </div>
         </div>
