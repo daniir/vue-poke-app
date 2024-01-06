92a79683b560a5a53f954f864e7d1b0d6a2b8c01
daniir <danrodas03@gmail.com>
2024-01-06 12:00:49 -0500

    Añadiendo Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia'
 import route from './router';
 import App from './App.vue';
 import './style.css';
 
-createApp(App).use(route).mount('#app')
+
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia)
+app.use(route)
+app.mount('#app')
