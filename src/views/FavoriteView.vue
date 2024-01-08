7f1f132e276d169c7833891372564b5fda734383
daniir <danrodas03@gmail.com>
2024-01-07 20:18:09 -0500

    Ajuste de vista de favoritos

--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { usePokemons } from "../composables/";
+import PokemonFavoriteList from "../components/pokemons/PokemonFavoriteList.vue";
 
 const { favorites } = usePokemons();
 
@@ -8,7 +9,7 @@ const { favorites } = usePokemons();
 <template>
     <div v-if="favorites.length > 0">
         <h1 class="text-center font-bold text-5xl my-5 text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 from-10% via-orange-500 via-30% to-red-500 to-90%">Mis Pokémons</h1>
-        <h1>Listado con datos</h1>
+        <PokemonFavoriteList />
     </div>
     <div class="flex flex-col justify-center items-center mt-14" v-else>
         <p class="text-4xl text-gray-500 mb-5">No se han añadido pokémons</p>
