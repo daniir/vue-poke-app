3714308258ede82647f975bdeb12255878f9d50e
daniir <danrodas03@gmail.com>
2024-01-07 21:42:03 -0500

    Componente de pie de página

---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="flex justify-center w-full bg-black text-white rounded-t-xl p-5 mt-5">
+        <div class="flex items-center">
+            <a href="https://vitejs.dev/">
+                <img src="../../../public/vite.svg" alt="Vite-logo" width="75">
+            </a>
+            <p class="text-5xl mx-2.5"> + </p>
+        </div>
+        <div class="flex items-center">
+            <a href="https://vuejs.org/">
+                <img src="../../assets/vue.svg" alt="Vue-logo" width="75">
+            </a>
+            <p class="text-5xl mx-2.5"> + </p>
+        </div>
+        <div>
+            <a href="https://pokeapi.co/">
+                <img src="../../assets/pokemon-logo.svg" alt="Pokemon-logo" width="200">
+            </a>
+        </div>
+    </footer>
+</template>
