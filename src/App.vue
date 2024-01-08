7ec6a5092b287ea517c2592fec7c1a146cb59526
daniir <danrodas03@gmail.com>
2024-01-07 21:41:36 -0500

    Añadiendo footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import NavBar from './components/shared/NavBar.vue';
+import Footer from './components/shared/Footer.vue';
 
 
 </script>
@@ -9,6 +10,7 @@ import NavBar from './components/shared/NavBar.vue';
   <div class="mx-2">
     <RouterView/>
   </div>
+  <Footer />
 </template>
 
 <style>
